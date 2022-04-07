60eb982119c424dc5f9536e3c0fd07e151f6bd48
Em <ekramm@me.com>
2022-04-07 14:46:49 -0400

    commit tree 8eb0610d668b19d8a90bca06073e6c14ceca6901

---
+++ index.js
@@ -0,0 +1,52 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+
+}
+
+function multiply(a, b) {
+    return a * b;
+
+}
+
+function divide(a, b) {
+    return a / b;
+
+}
+
+function increment(n) {
+    return ++n;
+}
+
+function decrement(n) {
+    return --n;
+}
+
+
+/*it('increment(n) increments n and returns the result', function() {
+    expect(increment(a)).toEqual(a + 1)
+  })
+  
+  it('decrement(n) decrements n and returns the result', function() {
+    expect(decrement(a)).toEqual(a - 1)
+  })
+  */
+
+
+
+
+function makeInt(string) {
+    parseInt(string, 10)
+        return parseInt(string, 10);
+
+}
+
+function preserveDecimal(string) {
+    parseFloat("2.222");
+    parseFloat("sldkjflksjf");
+        return parseFloat(string);
+
+}
